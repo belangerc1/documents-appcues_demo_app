6b7e37d63405a1093437fc8ee6d52fbd92983410
Caleb Belanger <caleb@appcues.com>
2026-09-23 16:30:55 -0400

    homepage copy added

--- src/Home.js
+++ src/Home.js
@@ -9,9 +9,16 @@ import squirrel from "./resources/squirrel.jpg"
 const Home = () => {
   return (
     <div className="app-content">
-      <h1>Welcome to this feature-rich test application</h1>
-      <h2>We're glad you're here</h2>
-      <img src={squirrel} alt="a cute little squirrel peering at you from the tall grass"></img>
+      <div className="home-header">
+        <h1>Welcome to this feature-rich test application</h1>
+        <h2>We're glad you're here</h2>
+        <img src={squirrel} alt="a cute little squirrel peering at you from the tall grass"></img>
+      </div>
+      <div className="home-body">
+        <p>
+          This application is designed to demonstrate a variety of features and capabilities within Appcues and to provide a sand box to test new functionality as well as our legacy features. Explore the different sections to see what this app can do!
+        </p>
+      </div>
     </div>
   );
 };
