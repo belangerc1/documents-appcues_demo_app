f7e9481249e7313cb38c73b8b50325ca045ee2fc
Caleb Belanger <caleb@appcues.com>
2026-09-23 16:34:47 -0400

    na

--- src/Home.js
+++ src/Home.js
@@ -10,11 +10,11 @@ const Home = () => {
   return (
     <div className="app-content">
       <div className="home-header">
-        <h1>Welcome to this feature-rich test application</h1>
-        <h2>We're glad you're here</h2>
         <img src={squirrel} alt="a cute little squirrel peering at you from the tall grass"></img>
       </div>
       <div className="home-body">
+        <h1>Welcome to this feature-rich test application</h1>
+        <h2>We're glad you're here</h2>
         <p>
           This application is designed to demonstrate a variety of features and capabilities within Appcues and to provide a sand box to test new functionality as well as our legacy features. Explore the different sections to see what this app can do!
         </p>
