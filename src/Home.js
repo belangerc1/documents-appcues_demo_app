8644738ef62de32ac2297083c913edab4e0fc997
Caleb Belanger <caleb@appcues.com>
2026-09-24 09:50:13 -0400

    Complete visual redesign

--- src/Home.js
+++ src/Home.js
@@ -1,5 +1,5 @@
 import React from "react";
-import appcueslogo from "./resources/appcueslogo.jpg"
+import appcueslogo from "./resources/appcueslogocolor-trimmed.png"
 
 /* 
   feel free to add/remove any HTML you would like. Just remember that all content
@@ -8,21 +8,23 @@ import appcueslogo from "./resources/appcueslogo.jpg"
 
 const Home = () => {
   return (
-    <div className="app-content">
-      <div className="home-header">
-        <img src={appcueslogo} alt="Appcues Logo"></img>
-      </div>
-      <div className="home-body">
-        <h1>Welcome to this feature-rich test application</h1>
-        <h2>We're glad you're here</h2>
-        <p>
-          This application is designed to demonstrate a variety of features and capabilities within Appcues and to provide a sand box to test new functionality as well as our legacy features. Explore the different sections to see what this app can do!
-        </p>
+    <div className="app-content home">
+      <div className="home-inner">
+        <img className="home-logo" src={appcueslogo} alt="Appcues Logo"></img>
+        <div className="home-text">
+          <div className="home-titles">
+            <h1>Welcome to The Appcues Test Application</h1>
+            <h2>We're glad you're here!</h2>
+          </div>
+          <div className="home-body">
+            <p>
+              This application is designed to demonstrate a variety of features and capabilities within Appcues and to provide a sand box to test new functionality as well as our legacy features. Explore the different sections to see what this app can do!
+            </p>
+          </div>
+        </div>
       </div>
     </div>
   );
 };
 
 export default Home;
-
-
--- src/Navigation.js
+++ src/Navigation.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import navLogo from "./resources/appcuesfavicon.png";
 
 /* 
   feel free to add/remove any HTML you would like. Just remember that all content
@@ -9,10 +10,13 @@ import { Link } from "react-router-dom";
 const Navigation = () => (
 
   <div id="navigation">
+    <Link to="/" className="nav-logo" aria-label="Home">
+      <img src={navLogo} alt="Appcues" />
+    </Link>
     <Link to="/">Home</Link>
     <Link to="/about">About</Link>
-    <Link to="/contact">Contact</Link>
     <Link to="/settings">Settings</Link>
+    <Link to="/contact">Contact</Link>
     <Link to="/login">Login</Link>
     <div id="launchpad"></div> 
   </div>
